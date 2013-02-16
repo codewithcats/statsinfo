8e1b4fe61bd1c7cebc5cd91179aa118043f98993
Tanawat Tassana <amp.tanawat@gmail.com>
2013-02-16 08:26:29 +0700

    added ProductGroup DbSet

--- StatsInfoSystem/StsContext.cs
+++ StatsInfoSystem/StsContext.cs
@@ -5,5 +5,6 @@ namespace StatsInfoSystem
     public class StsContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductGroup> ProductGroups { get; set; }
     }
 }
