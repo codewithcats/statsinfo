5e56cb7029abd3dc7dcf4abcff1d6e2c931859d7
Tanawat Tassana <amp.tanawat@gmail.com>
2013-02-16 07:41:07 +0700

    added Product module

---
+++ StatsInfoSystem/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StatsInfoSystem
+{
+    class ProductGroup
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+    class ProductCategory
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+    class Product
+    {
+        public string Code { get; set; }
+        public string NameTh { get; set; }
+        public string NameEn { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public ProductCategory Category { get; set; }
+        public ProductGroup group { get; set; }
+    }
+}
