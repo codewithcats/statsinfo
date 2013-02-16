5d62c23b49c8be0e2affbb2ba3dc30acf732395e
Tanawat Tassana <amp.tanawat@gmail.com>
2013-02-16 08:16:20 +0700

    added STS Console application

---
+++ StsConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
