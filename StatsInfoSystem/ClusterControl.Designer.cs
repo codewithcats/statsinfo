e7c7606c52ae4d8c1dbadd569691e2e69726dbe0
Thassaya <newpomelo@hotmail.com>
2013-03-30 18:59:00 +0700

    แก้ ชื่อวิธีใน Cluster

--- StatsInfoSystem/ClusterControl.Designer.cs
+++ StatsInfoSystem/ClusterControl.Designer.cs
@@ -30,6 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClusterControl));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.label170 = new System.Windows.Forms.Label();
+            this.holtRadio = new System.Windows.Forms.RadioButton();
+            this.simpleRadio = new System.Windows.Forms.RadioButton();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -256,17 +263,13 @@
             this.customerList = new System.Windows.Forms.ListBox();
             this.comboBox6 = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.comboBox7 = new System.Windows.Forms.ComboBox();
-            this.label170 = new System.Windows.Forms.Label();
-            this.holtRadio = new System.Windows.Forms.RadioButton();
-            this.simpleRadio = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.panel2.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -279,9 +282,6 @@
             this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tabPage3.SuspendLayout();
-            this.groupBox3.SuspendLayout();
-            this.panel3.SuspendLayout();
-            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -302,6 +302,84 @@
             this.splitContainer1.Size = new System.Drawing.Size(1014, 540);
             this.splitContainer1.SplitterDistance = 362;
             this.splitContainer1.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.panel3);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(4, 171);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox3.Size = new System.Drawing.Size(294, 117);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "เลือกเทคนิคที่ใช้จัดกลุ่ม";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.holtRadio);
+            this.panel3.Controls.Add(this.simpleRadio);
+            this.panel3.Location = new System.Drawing.Point(7, 18);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(272, 91);
+            this.panel3.TabIndex = 8;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.comboBox7);
+            this.panel4.Controls.Add(this.label170);
+            this.panel4.Location = new System.Drawing.Point(45, 46);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 37);
+            this.panel4.TabIndex = 8;
+            this.panel4.Visible = false;
+            // 
+            // comboBox7
+            // 
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "ทั้งหมด"});
+            this.comboBox7.Location = new System.Drawing.Point(103, 9);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(82, 21);
+            this.comboBox7.TabIndex = 16;
+            // 
+            // label170
+            // 
+            this.label170.AutoSize = true;
+            this.label170.Location = new System.Drawing.Point(12, 12);
+            this.label170.Name = "label170";
+            this.label170.Size = new System.Drawing.Size(85, 13);
+            this.label170.TabIndex = 15;
+            this.label170.Text = "จำนวนกลุ่มลูกค้า";
+            // 
+            // holtRadio
+            // 
+            this.holtRadio.AutoSize = true;
+            this.holtRadio.Location = new System.Drawing.Point(30, 27);
+            this.holtRadio.Name = "holtRadio";
+            this.holtRadio.Size = new System.Drawing.Size(157, 17);
+            this.holtRadio.TabIndex = 1;
+            this.holtRadio.Text = "Hierarchical Cluster Analysis";
+            this.holtRadio.UseVisualStyleBackColor = true;
+            // 
+            // simpleRadio
+            // 
+            this.simpleRadio.AutoSize = true;
+            this.simpleRadio.Checked = true;
+            this.simpleRadio.Location = new System.Drawing.Point(30, 4);
+            this.simpleRadio.Name = "simpleRadio";
+            this.simpleRadio.Size = new System.Drawing.Size(143, 17);
+            this.simpleRadio.TabIndex = 0;
+            this.simpleRadio.TabStop = true;
+            this.simpleRadio.Text = "K-Means Cluster Analysis";
+            this.simpleRadio.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
@@ -2792,84 +2870,6 @@
             this.label6.TabIndex = 0;
             this.label6.Text = "เลือกกลุ่มลูกค้า";
             // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.panel3);
-            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox3.Location = new System.Drawing.Point(4, 171);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox3.Size = new System.Drawing.Size(294, 117);
-            this.groupBox3.TabIndex = 8;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "เลือกเทคนิคที่ใช้จัดกลุ่ม";
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.panel4);
-            this.panel3.Controls.Add(this.holtRadio);
-            this.panel3.Controls.Add(this.simpleRadio);
-            this.panel3.Location = new System.Drawing.Point(7, 18);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(236, 91);
-            this.panel3.TabIndex = 8;
-            // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.comboBox7);
-            this.panel4.Controls.Add(this.label170);
-            this.panel4.Location = new System.Drawing.Point(18, 46);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(200, 37);
-            this.panel4.TabIndex = 8;
-            this.panel4.Visible = false;
-            // 
-            // comboBox7
-            // 
-            this.comboBox7.FormattingEnabled = true;
-            this.comboBox7.Items.AddRange(new object[] {
-            "1",
-            "2",
-            "3",
-            "4",
-            "ทั้งหมด"});
-            this.comboBox7.Location = new System.Drawing.Point(103, 9);
-            this.comboBox7.Name = "comboBox7";
-            this.comboBox7.Size = new System.Drawing.Size(82, 21);
-            this.comboBox7.TabIndex = 16;
-            // 
-            // label170
-            // 
-            this.label170.AutoSize = true;
-            this.label170.Location = new System.Drawing.Point(12, 12);
-            this.label170.Name = "label170";
-            this.label170.Size = new System.Drawing.Size(85, 13);
-            this.label170.TabIndex = 15;
-            this.label170.Text = "จำนวนกลุ่มลูกค้า";
-            // 
-            // holtRadio
-            // 
-            this.holtRadio.AutoSize = true;
-            this.holtRadio.Location = new System.Drawing.Point(3, 27);
-            this.holtRadio.Name = "holtRadio";
-            this.holtRadio.Size = new System.Drawing.Size(143, 17);
-            this.holtRadio.TabIndex = 1;
-            this.holtRadio.Text = "K-Means Cluster Analysis";
-            this.holtRadio.UseVisualStyleBackColor = true;
-            // 
-            // simpleRadio
-            // 
-            this.simpleRadio.AutoSize = true;
-            this.simpleRadio.Checked = true;
-            this.simpleRadio.Location = new System.Drawing.Point(3, 4);
-            this.simpleRadio.Name = "simpleRadio";
-            this.simpleRadio.Size = new System.Drawing.Size(157, 17);
-            this.simpleRadio.TabIndex = 0;
-            this.simpleRadio.TabStop = true;
-            this.simpleRadio.Text = "Hierarchical Cluster Analysis";
-            this.simpleRadio.UseVisualStyleBackColor = true;
-            // 
             // ClusterControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -2881,6 +2881,11 @@
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
@@ -2901,11 +2906,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tabPage3.ResumeLayout(false);
             this.tabPage3.PerformLayout();
-            this.groupBox3.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
-            this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
